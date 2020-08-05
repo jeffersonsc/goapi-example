a8c895158a5f3e39745baf42c39fe517f04a6a3b
Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>
2020-08-05 17:45:58 -0300

    Create mongo data structure and repository
    Signed-off-by: Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/jeffersonsc/natureapi
 
 go 1.13
 
-require go.mongodb.org/mongo-driver v1.4.0
+require (
+	go.mongodb.org/mongo-driver v1.4.0
+	gonum.org/v1/gonum v0.7.0 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
+)
--- pkg/product/mongo.go
+++ pkg/product/mongo.go
@@ -2,8 +2,12 @@ package product
 
 import (
 	"context"
+	"time"
 
+	"github.com/jeffersonsc/natureapi"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
 )
 
 const collectionName = "products"
@@ -28,16 +32,94 @@ func NewMongoRepository(ctx context.Context, client *mongo.Client) Repository {
 }
 
 // FindAll products in databse
-func (r *MongoRepository) FindAll(filter map[string]interface{}) ([]*Product, error) { return nil, nil }
+func (r *MongoRepository) FindAll(filter map[string]interface{}) ([]*Product, error) {
+	coll := r.client.Database(natureapi.DBNAME).Collection(GetCollectionName())
+
+	ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+	defer cancel()
+
+	cursor, err := coll.Find(ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+
+	products := []*Product{}
+	err = cursor.All(ctx, &products)
+	if err != nil {
+		return nil, err
+	}
+
+	return products, nil
+}
 
 // Find product by id
-func (r *MongoRepository) Find(id string) (*Product, error) { return nil, nil }
+func (r *MongoRepository) Find(id string) (*Product, error) {
+	coll := r.client.Database(natureapi.DBNAME).Collection(GetCollectionName())
+
+	ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+	defer cancel()
+
+	objID, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.M{"_id": bson.M{"$eq": objID}}
+	product := Product{}
+	err = coll.FindOne(ctx, filter).Decode(&product)
+	if err != nil {
+		return nil, err
+	}
+
+	return &product, nil
+}
 
 // Create a new product or update product has created
-func (r *MongoRepository) Create(product *Product) error { return nil }
+func (r *MongoRepository) Create(product *Product) error {
+	coll := r.client.Database(natureapi.DBNAME).Collection(GetCollectionName())
+
+	ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+	defer cancel()
+
+	result, err := coll.InsertOne(ctx, product)
+
+	if err != nil {
+		return err
+	}
+
+	product.ID = result.InsertedID.(primitive.ObjectID)
+
+	return nil
+}
 
 // Update a project in database
-func (r *MongoRepository) Update(product *Product) error { return nil }
+func (r *MongoRepository) Update(product *Product) error {
+	coll := r.client.Database(natureapi.DBNAME).Collection(GetCollectionName())
+
+	ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+	defer cancel()
+
+	filter := bson.M{"_id": bson.M{"$eq": product.ID}}
+	_, err := coll.UpdateOne(ctx, filter, bson.M{"$set": product})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 // Delete project database WARN!!!
-func (r *MongoRepository) Delete(product *Product) error { return nil }
+func (r *MongoRepository) Delete(product *Product) error {
+	coll := r.client.Database(natureapi.DBNAME).Collection(GetCollectionName())
+
+	ctx, cancel := context.WithTimeout(r.ctx, time.Second*10)
+	defer cancel()
+
+	filter := bson.M{"_id": bson.M{"$eq": product.ID}}
+	_, err := coll.DeleteOne(ctx, filter)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
