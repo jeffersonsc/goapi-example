4e9d07c96910323e1527c9617124b07fafe5164c
Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>
2020-08-06 08:02:27 -0300

    Add validation connection in mongodb
    Signed-off-by: Jefferson Silva <jefferson.silva@assertivasolucoes.com.br>

--- internal/db/mongo.go
+++ internal/db/mongo.go
@@ -6,12 +6,23 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-// NewConn crete a new connection with mongodb
-func NewConn(ctx context.Context, uri string) (*mongo.Client, error) {
+// NewMongoConn crete a new connection with mongodb
+func NewMongoConn(ctx context.Context, uri string) (*mongo.Client, error) {
 	cCtx, cancel := context.WithTimeout(ctx, time.Second*10)
 	defer cancel()
 
-	return mongo.Connect(cCtx, options.Client().ApplyURI(uri))
+	client, err := mongo.Connect(cCtx, options.Client().ApplyURI(uri))
+	if err != nil {
+		return nil, err
+	}
+
+	err = client.Ping(cCtx, readpref.Primary())
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
 }
